146c4ab9f822a1a9c7764ac96f351f1bb4828632
mmelgies <michalmelgies95@gmail.com>
2024-03-16 01:05:54 +0100

    Write a function min(a,b) which returns the least of two numbers a and b

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
-/*function showMessage(from, text = anotherFunction()) {
-  alert( from + ": " + text );
-}
-/*
-showMessage("Ann"); 
-
-
-function anotherFunction(){
-  alert("hello");
-}
-*/
-
-/*function checkAge(age) {
-  if (age >= 18) {
-    return true;
-  } else {
-    return confirm('Do you have permission from your parents?');
+function leastOf(a,b){
+  if (a > b) {
+    alert(b);
+    return b;
+  }
+  else {
+    alert(a);
+    return a;
   }
 }
 
-let age = prompt('How old are you?', 18);
-
-if ( checkAge(age) ) {
-  alert( 'Access granted' );
-} else {
-  alert( 'Access denied' );
-}
-*/
-function checkAge(age) {
-  return (age > 18) || confirm('Did parents allow you?');
-}
-
-checkAge(19);
+leastOf(4,1);
