87e554db3d0015fdc152840d70724b71d131613a
mmelgies <michalmelgies95@gmail.com>
2024-03-16 01:15:42 +0100

    Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-function leastOf(a,b){
-  if (a > b) {
-    alert(b);
-    return b;
-  }
-  else {
-    alert(a);
-    return a;
-  }
+function pow(x, n){ //my version
+  alert(Math.pow(x,n));
+  return Math.pow(x,n);
 }
 
-leastOf(4,1);
+pow(3,2);
+
+
+/*versions from solution:
+function pow(x, n) {
+  let result = x;
+
+  for (let i = 1; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
+
+let x = prompt("x?", '');
+let n = prompt("n?", '');
+
+if (n < 1) {
+  alert(`Power ${n} is not supported, use a positive integer`);
+} else {
+  alert( pow(x, n) );
+}
+*/
