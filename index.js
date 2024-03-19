991be24d05cbdad10987447634eb4b161ed80027
mmelgies <michalmelgies95@gmail.com>
2024-03-19 09:21:29 +0100

    breakak with odin for javascript.info

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
-function pow(x, n){ //my version
+/*function pow(x, n){ //my version
   alert(Math.pow(x,n));
   return Math.pow(x,n);
 }
 
-pow(3,2);
+pow(3,2);*/
 
-
-/*versions from solution:
-function pow(x, n) {
-  let result = x;
-
-  for (let i = 1; i < n; i++) {
-    result *= x;
-  }
-
-  return result;
-}
-
-let x = prompt("x?", '');
-let n = prompt("n?", '');
-
-if (n < 1) {
-  alert(`Power ${n} is not supported, use a positive integer`);
-} else {
-  alert( pow(x, n) );
-}
-*/
